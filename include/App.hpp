c53874ca3b51051fcf32f0e7bb53dbab50db68dd
Erik Wessel <44678770+DunkleMango@users.noreply.github.com>
2020-04-09 18:08:55 +0200

    init App

---
+++ include/App.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <GLRF/Scene.hpp>
+#include <GLRF/SceneCamera.hpp>
+#include <GLRF/SceneObject.hpp>
+#include <GLRF/SceneLight.hpp>
+#include <GLRF/Shader.hpp>
+#include <GLRF/VertexFormat.hpp>
+#include <GLRF/PlaneGenerator.hpp>
+#include <GLRF/Material.hpp>
